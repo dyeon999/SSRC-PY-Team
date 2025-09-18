3beabc500b83180cfc3fe9d03a585895ef938e3f
ahneunseon <97dmstjs2@gmail.com>
2025-09-18 10:29:06 +0900

    코드 수정

--- test.py
+++ test.py
@@ -5,11 +5,15 @@ from email.mime.text import MIMEText
 import schedule                                                          
 import time                                                                                          
 
+# DB 접속 및 연결
+
 client = MongoClient("mongodb://localhost:27017/")             
 db = client["shodan_db"]                                                                    
 collection = db["webcam_results"]                              
 
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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" 
+# 슬랙 설정
+
+SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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" 
 
 def slack_upload(message):                                            
     payload = {"text": message}              
@@ -19,14 +23,15 @@ def slack_upload(message):
     else:
         print(f"Slack 전송에 성공하였습니다.: {message}")
 
+# 이메일 설정
 
 SMTP_SERVER = "smtp.gmail.com"                   
 SMTP_PORT = 587          
 EMAIL_USER = "97dmstjs2@gmail.com"           
 EMAIL_PASS = ""                                   
-TO_EMAIL = "cile0629@naver.com"
+TO_EMAIL = ["cile0629@naver.com", ""]
 
-def send_email(subject, body):        
+def email_upload(subject, body):        
     msg = MIMEText(body, "plain")                                                                       
     msg["Subject"] = subject
     msg["From"] = EMAIL_USER
@@ -45,18 +50,18 @@ def alert():
         msg = f"열린 포트를 발견했습니다.\nIP: {r['ip']}\nPort: {r['port']}\nOrg: {r.get('org', 'N/A')}\nlocation: {r['location']}"
 
         slack_upload(msg)
-        send_email("[경고] 포트가 열려있습니다.", msg)
+        email_upload("[경고] 포트가 열려있습니다.", msg)
 
     print("알림 발송을 완료했습니다.")
 
 
 # 작업 예약 기능 (5초(?)마다 실행)
 
-schedule.every(1).minutes.do(alert)
+schedule.every(5).minutes.do(alert)
 
 alert()
 
-print("자동화를 시작합니다.")
+print("자동 알림 전송을 시작합니다.")
 while True:
     schedule.run_pending()
     time.sleep(1)
