7794b6f0569210813c77ea13075df0763c56449f
ahneunseon <97dmstjs2@gmail.com>
2025-09-18 13:39:15 +0900

    환경변수 추가

---
+++ testtesttesttest.py
@@ -0,0 +1,13 @@
+SHODAN_API_KEY=hSJjNorx0JWBIjO13ARsncSGLdVqMUeU
+
+mongodb=mongodb://localhost:27017/
+db_name=shodan_db
+collec_name=totals
+
+SLACK_WEBHOOK_URL=https://hooks.slack.com/services/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
+
+SMTP_SERVER=smtp.gmail.com
+SMTP_PORT=587
+SECRET_ID=97dmstjs2@gmail.com
+SECRET_PASS=otwmxsvubczbepth
+TO_EMAIL=97dmstjs@naver.com
